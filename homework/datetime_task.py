8d70921e02b25aeb942a4817600a8c885012f5d8
Roman Nowichkow <73201690+Porpoising@users.noreply.github.com>
2021-09-20 18:37:56 +0300

    Delete homework directory

--- homework/datetime_task.py
+++
@@ -1,6 +0,0 @@
-import datetime
-date_list = [int(i) for i in input().split()]
-date1 = datetime.date(date_list[0], date_list[1], date_list[2])
-days = datetime.timedelta(int(input()))
-date2 = date1 + days
-print(date2.year, date2.month, date2.day)
